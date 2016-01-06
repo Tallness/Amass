1dfea0811048d6a8bb4dc2bd90a6a5a21e9ff0ab
Steve McKnight <stephen.mcknight@gmail.com>
2016-01-05 22:06:28 -0500

    Cleanup after rename and break-out
    Cleanup of solution and project files after rename of Model project and
    break out of GameAction classes.

--- ConsoleSampler/Program.cs
+++ ConsoleSampler/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Model;
 using Amass.Engine;
+using Amass.Model;
 
 
 namespace ConsoleSampler
